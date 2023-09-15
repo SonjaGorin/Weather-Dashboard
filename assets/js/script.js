73caaf12f1aaaf80d1e15c65e290b55a448c57b2
Sonja <sonjaljatkovic@gmail.com>
2023-09-15 15:29:19 -0400

    created li elements for searched cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lo
 var apiKey = "347c279390e03c0864320067fefb8c47"
 var cityInputEl = document.querySelector("#city-input")
 var searchButtonEl = document.querySelector("#search-button")
+var searchedCitiesUl = document.querySelector(".searched-cities")
 
 
 
@@ -11,19 +12,35 @@ var storeCities = function() {
     localStorage.setItem("searchedCitiesList", JSON.stringify(searchedCitiesList));
 }
 
-var displaySearchedCities = function(event) {
+var addCityToList = function(event) {
     event.preventDefault();
-    var cityInput = cityInputEl.value.trim()
-
-    if (cityInput === "") {
+    var cityInput = cityInputEl.value.trim();
+    var cityInputUpper = cityInput[0].toUpperCase() + cityInput.slice(1);
+    if (cityInputUpper === "") {
         return;
     }
-
-    searchedCitiesList.push(cityInput)
-
+    for (i = 0; i < searchedCitiesList.length; i++) {
+        if (cityInputUpper === searchedCitiesList[i]) {
+            cityInputEl.value = "";
+            return;            
+        }
+    }
+    searchedCitiesList.push(cityInputUpper);
 
     storeCities();
-    cityInputEl.value = ""
+    renderPastSearches();
+    cityInputEl.value = "";
 }
 
-searchButtonEl.addEventListener("click", displaySearchedCities);
+var renderPastSearches = function() {
+    for (i = 0; i < searchedCitiesList.length; i++) {
+        var citiesListEl = document.createElement("li");
+        citiesListEl.textContent = searchedCitiesList[i];
+    }
+    searchedCitiesUl.appendChild(citiesListEl);
+}
+
+
+
+
+searchButtonEl.addEventListener("click", addCityToList);
